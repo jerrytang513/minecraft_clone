765d0d8209c3b4237e084150fe78b1c8bf3d7c2b
innkeeper <jerrytang513@hotmail.com>
2018-06-11 22:05:34 -0400

    Clean it up prepare for rebuild

--- src/graphics/asset/allShape.h
+++
@@ -1,7 +0,0 @@
-#ifndef ALLSHAPE_H_
-#define ALLSHAPE_H_
-
-#include "shape3d.h"
-#include "cube.h"
-
-#endif
--- src/math/utilities.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "vec3d.h"
-#include "vec4d.h"
-
-inline float toRadians(float degrees){
-    return degrees * (M_PI / 180);
-}
